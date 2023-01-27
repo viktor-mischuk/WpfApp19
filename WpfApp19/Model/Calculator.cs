b92952cb29ba54f81e308e5dcfc266eef0e2835d
VIC <mvv0864@gmail.com>
2023-01-27 16:09:41 +0300

    Добавьте файлы проекта.

---
+++ WpfApp19/Model/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp19.Model
+{
+    static class Calculator
+    {
+        public static double CircleLength(double r)
+        {
+            return 3.14 * 2 * r;
+        }
+    }
+}
---
+++ WpfApp19/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp19.Model;
+
+namespace WpfApp19.ViewModel
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private double radius;
+        public double Radius 
+        {
+            get => radius;
+            set
+            { 
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+        private double cLength;
+        public double CLength
+        {
+            get => cLength;
+            set
+            {
+                cLength = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CircleLengthCommand { get; }
+        private void OnCircleLengthCommandExecute(object parameter)
+        {
+            CLength = Calculator.CircleLength(Radius);
+        }
+        private bool CanCircleLengthCommandExecuted(object parameter)
+        {
+            if (Radius > 0) return true;
+            return false;
+        }
+        
+        public MainWindowViewModel()
+        {
+            CircleLengthCommand = new RelayCommand(OnCircleLengthCommandExecute, CanCircleLengthCommandExecuted);
+        }
+    }
+}
